f1f1407805e7542290ef65e6c9130b597a88ef8c
marcin <kirinson321@gmail.com>
2018-03-09 09:03:24 +0100

    minor cleanup, according to standards

--- php-test/test.php
+++ php-test/test.php
@@ -1,82 +1,114 @@
 <?php
 
-    interface MoneyFormatter
+//interface MoneyFormatter
+//{
+//    public function format(Money $object, string $decimal, string $thousands): string;
+//}
+//
+//class Formatter implements MoneyFormatter
+//{
+//    public function format(Money $object, string $decimal, string $thousands): string
+//    {
+//        $output = number_format($object->showAmount(), 2, $decimal, $thousands) . " " . $object->showCurrency();
+//        return $output;
+//    }
+//}
+
+
+interface MoneyFormatter
+{
+    public function format(Money $object);
+}
+
+class CurrencyFormatter implements MoneyFormatter
+{
+
+    /**
+     * CurrencyFormatter constructor.
+     */
+    public function __construct(string $decimal, string $thousands)
     {
-        public function format(Money $object, string $decimal, string $thousands): string;
+        $this->decimal = $decimal;
+        $this->thousands = $thousands;
     }
 
-    class Formatter implements MoneyFormatter
+    public function format(Money $object)//, string $decimal, string $thousands)
     {
-        public function format(Money $object, string $decimal, string $thousands): string
-        {
-            $output = number_format($object->showAmount(), 2, $decimal, $thousands) . " " . $object->showCurrency();
-            return $output;
-        }
+        // TODO: Implement format() method.
+        $output = number_format($object->showAmount(), 2, $this->decimal, $this->thousands);
+        return $output;
+    }
+}
+
+class Money extends CurrencyFormatter
+{
+    private $currency;
+    private $amount;
+
+    public function __construct(string $currency, float $amount)
+    {
+        $this->currency = $currency;
+        $this->amount = $amount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmount(): float
+    {
+        return $this->amount;
     }
 
 
-    class Money extends Formatter
+    public function multiplyMoney(int $times)
     {
-        private $currency;
-        private $amount;
-
-        public function __construct(string $currency, float $amount)
-        {
-            $this->currency = $currency;
-            $this->amount = $amount;
-        }
-
-        public function showAmount(): float
-        {
-            return $this->amount;
-        }
-
-        public function showCurrency(): string
-        {
-            return $this->currency;
-        }
-
-        public function multiplyMoney(int $times)
-        {
-            $this->amount = $this->amount*$times;
-        }
-
-        public function divideMoney(int $times)
-        {
-            $this->amount = $this->amount / $times;
-        }
-
-        public function addMoney(Money $second)
-        {
-            if($this->currency != $second->currency)
-            {
-                throw new Exception("Kwoty powinny być w tych samych walutach");
-            }
-            $this->amount = $this->amount + $second->amount;
-        }
-
-        public function subtractMonet(Money $second): float
-        {
-            if ($this->currency != $second->currency) {
-                throw new Exception("Kwoty powinny być w tych samych walutach");
-            }
-            $this->amount = $this->amount - $second->amount;
-        }
+        $this->amount = $this->amount * $times;
     }
 
-
-    if(gettype($argv[1]) != "string")
+    public function divideMoney(int $times)
     {
-        throw new Exception("Jako pierwszy argument wywołania programu powinna zostać podana kwota");
+        $this->amount = $this->amount / $times;
     }
 
-    $sum = new Money($argv[1], 0.00);
-    for($i=2; $i<$argc; $i++)
+    public function addMoney(Money $second): void
     {
-        $second = new Money($argv[1], $argv[$i]);
-        $sum->addMoney($second);
+        if ($this->currency != $second->currency) {
+            throw new Exception("Kwoty powinny być w tych samych walutach");
+        }
+
+        $this->amount = $this->amount + $second->amount;
     }
 
-    echo $sum->format($sum, '.', ' ');
+    public function subtractMonet(Money $second)
+    {
+        if ($this->currency != $second->currency) {
+            throw new Exception("Kwoty powinny być w tych samych walutach");
+        }
 
-?>
+        $this->amount = $this->amount - $second->amount;
+    }
+}
+
+
+if (gettype($argv[1]) != "string") {
+    throw new Exception("Jako pierwszy argument wywołania programu powinna zostać podana kwota");
+}
+
+$sum = new Money($argv[1], 0.00);
+
+for ($i = 2; $i < $argc; $i++) {
+    $second = new Money($argv[1], $argv[$i]);
+    $sum->addMoney($second);
+}
+
+echo $sum->format($sum, '.', ' ');
+
+
