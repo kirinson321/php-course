645f65c7c11441d6d1b993da248d24e7510c5bd3
marcin <kirinson321@gmail.com>
2018-03-23 04:13:19 +0100

    add POST handling (damn got a bit dizzy bout damn postman)

--- first-server/web/index.php
+++ first-server/web/index.php
@@ -2,11 +2,13 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 use Money\Money;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 #echo "dupa";
 
 $app = new \Silex\Application();
-#$app['debug'] = true;
+$app['debug'] = true;
 
 $products_list = scandir("products/");
 $products_list = array_diff($products_list, array('.', '..'));
@@ -28,11 +30,28 @@ $app->get('products/{id}', function (Silex\Application $app, $id) use ($products
        $app->abort(404, "Product with id $id does not exist");
    }
 
-   $product = $products_list[$id];
-   //$output = var_dump($products_list);
+    //$product = $products_list[$id];
+    //$output = var_dump($products_list);
     $filename = $products_list[$id];
     return $filename;
-   #return $product;
+    //return $product;
+});
+
+$app->post('/products', function (Request $request) use ($products_list) {
+
+    $data = $request->request->all();
+    $output = '';
+
+    foreach($data as $key=>$value){
+        if(isset($products_list[$key])){
+            return new Response("Resource already exists", 409);
+        }
+
+        file_put_contents("products/product" . $key, $value);
+        $output .= $key . " ";
+    }
+
+    return new Response("Created files with ids: $output", 201);
 });
 
 $app->delete('/products/{id}', function ($id, Silex\Application $app) use ($products_list) {
